fdc661c5542996a265f101452eb6bb1b9d6fd47f
RhinoLee <picklerhino@gmail.com>
2021-03-31 14:21:21 +0800

    validate message with Joi & insert message into DB & respond with inserted message

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+const messages = require('./db/messages')
 
 const app = express()
 
@@ -14,6 +15,24 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.json({
     message: 'message board'
+  })
+})
+
+app.get('/messages', (req, res) => {
+  messages.getAll().then( messages => {
+    res.json(messages)
+  })
+})
+
+app.post('/messages', (req, res) => {
+  console.log(req.body);
+  messages.create(req.body)
+  .then( message => {
+    res.json(message)
+  })
+  .catch(err => {
+    res.status(500)
+    res.json(err)
   })
 })
 
